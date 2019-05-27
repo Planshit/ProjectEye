96e141384cb696b53e6798a290f54ee62cb0e673
Little panda <root@thelittlepandaisbehind.com>
2019-05-27 17:55:32 +0800

    统计数据模型

---
+++ src/ProjectEye/Core/Models/Statistic/StatisticListModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProjectEye.Core.Models.Statistic
+{
+    [XmlRootAttribute("StatisticList")]
+    public class StatisticListModel
+    {
+        public List<StatisticModel> Data { get; set; }
+    }
+}
