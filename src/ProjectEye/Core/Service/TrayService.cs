0f708d72ab648a45a936a742c3269801577a6885
Little panda <root@thelittlepandaisbehind.com>
2019-06-06 11:30:31 +0800

    美化托盘菜单样式

--- src/ProjectEye/Core/Service/TrayService.cs
+++ src/ProjectEye/Core/Service/TrayService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using System.Windows.Resources;
 
@@ -14,16 +16,21 @@ namespace ProjectEye.Core.Service
     /// </summary>
     public class TrayService : IService
     {
+        //托盘图标
         private System.Windows.Forms.NotifyIcon notifyIcon;
-        private System.Windows.Forms.ContextMenu contextMenu;
-        private System.ComponentModel.IContainer components;
+      
+        //Service
         private readonly App app;
         private readonly MainService mainService;
         private readonly ConfigService config;
+
         //托盘菜单项
-        private System.Windows.Forms.MenuItem menuItem_norest;
-        private System.Windows.Forms.MenuItem menuItem_sound;
-        private System.Windows.Forms.MenuItem menuItem_Statistic;
+        private ContextMenu contextMenu;
+        private MenuItem menuItem_NoReset;
+        private MenuItem menuItem_Sound;
+        private MenuItem menuItem_Statistic;
+        private MenuItem menuItem_Options;
+        private MenuItem menuItem_Quit;
         public TrayService(App app, MainService mainService, ConfigService config)
         {
             this.app = app;
@@ -31,11 +38,62 @@ namespace ProjectEye.Core.Service
             this.config = config;
             this.config.Changed += new EventHandler(config_Changed);
 
-            
             app.Exit += new ExitEventHandler(app_Exit);
         }
 
-        private void menuItem_menuItem_Statistic_Click(object sender, EventArgs e)
+        #region Init
+        public void Init()
+        {
+            //托盘菜单
+            contextMenu = new ContextMenu();
+            App.Current.Deactivated += (e, c) =>
+            {
+                contextMenu.IsOpen = false;
+            };
+            //托盘菜单项
+            menuItem_Statistic = new MenuItem();
+            menuItem_Statistic.Header = "查看数据统计";
+            menuItem_Statistic.Visibility = config.options.General.Data ? Visibility.Visible : Visibility.Collapsed;
+            menuItem_Statistic.Click += menuItem_Statistic_Click;
+
+            menuItem_Options = new MenuItem();
+            menuItem_Options.Header = "选项";
+            menuItem_Options.Click += menuItem_Options_Click;
+
+            menuItem_NoReset = new MenuItem();
+            menuItem_NoReset.Header = "不要提醒我";
+            menuItem_NoReset.Click += menuItem_NoRest_Click;
+
+            menuItem_Sound = new MenuItem();
+            menuItem_Sound.Header = "提示音";
+            menuItem_Sound.IsChecked= config.options.General.Sound;
+            menuItem_Sound.Click += menuItem_Sound_Click;
+
+            menuItem_Quit = new MenuItem();
+            menuItem_Quit.Header = "退出";
+            menuItem_Quit.Click += menuItem_Exit_Click;
+
+            //添加托盘菜单项
+            contextMenu.Items.Add(menuItem_Statistic);
+            contextMenu.Items.Add(menuItem_Options);
+            contextMenu.Items.Add(new Separator());
+            contextMenu.Items.Add(menuItem_NoReset);
+            contextMenu.Items.Add(menuItem_Sound);
+            contextMenu.Items.Add(new Separator());
+            contextMenu.Items.Add(menuItem_Quit);
+
+
+            //托盘图标添加
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            UpdateIcon("sunglasses");
+            notifyIcon.Text = "Project Eye";
+            notifyIcon.Visible = true;
+            notifyIcon.MouseClick += notifyIcon_MouseClick;
+        }
+        #endregion
+
+        #region Events
+        private void menuItem_Statistic_Click(object sender, EventArgs e)
         {
             WindowManager.CreateWindowInScreen("StatisticWindow");
             WindowManager.Show("StatisticWindow");
@@ -43,12 +101,12 @@ namespace ProjectEye.Core.Service
 
         private void config_Changed(object sender, EventArgs e)
         {
-            menuItem_norest.Checked = config.options.General.Noreset;
-            menuItem_sound.Checked = config.options.General.Sound;
-            menuItem_Statistic.Visible = config.options.General.Data;
+            menuItem_NoReset.IsChecked = config.options.General.Noreset;
+            menuItem_Sound.IsChecked = config.options.General.Sound;
+            menuItem_Statistic.Visibility = config.options.General.Data ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        private void menuItem_options_Click(object sender, EventArgs e)
+        private void menuItem_Options_Click(object sender, EventArgs e)
         {
             WindowManager.CreateWindowInScreen("OptionsWindow");
             WindowManager.Show("OptionsWindow");
@@ -56,78 +114,32 @@ namespace ProjectEye.Core.Service
 
 
 
-        private void menuItem_sound_Click(object sender, EventArgs e)
+        private void menuItem_Sound_Click(object sender, EventArgs e)
         {
-            var item = sender as System.Windows.Forms.MenuItem;
-            item.Checked = !item.Checked;
-            config.options.General.Sound = item.Checked;
+            var item = sender as MenuItem;
+            item.IsChecked = !item.IsChecked;
+            config.options.General.Sound = item.IsChecked;
             config.Save();
         }
 
-        public void Init()
+        private void notifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
         {
-            components = new System.ComponentModel.Container();
-            contextMenu = new System.Windows.Forms.ContextMenu();
-            //不要提醒我
-            menuItem_norest = new System.Windows.Forms.MenuItem();
-            menuItem_norest.Text = "不要提醒我";
-            menuItem_norest.Click += new System.EventHandler(menuItem_norest_Click);
-            //声音提示
-            menuItem_sound = new System.Windows.Forms.MenuItem();
-            menuItem_sound.Checked = config.options.General.Sound;
-            menuItem_sound.Text = "提示音";
-            menuItem_sound.Click += new System.EventHandler(menuItem_sound_Click);
-            //退出菜单项
-            System.Windows.Forms.MenuItem menuItem_exit = new System.Windows.Forms.MenuItem();
-            menuItem_exit.Text = "退出";
-            menuItem_exit.Click += new System.EventHandler(menuItem_exit_Click);
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                //右键单击弹出托盘菜单
+                contextMenu.IsOpen = true;
+                //激活主窗口，用于处理关闭托盘菜单
+                App.Current.MainWindow.Activate();
 
-
-
-
-            //选项
-            System.Windows.Forms.MenuItem menuItem_options = new System.Windows.Forms.MenuItem();
-            menuItem_options.Text = "选项";
-            menuItem_options.Click += new EventHandler(menuItem_options_Click);
-
-            //查看数据统计
-            menuItem_Statistic = new System.Windows.Forms.MenuItem();
-            menuItem_Statistic.Text = "查看数据统计";
-            menuItem_Statistic.Click += new EventHandler(menuItem_menuItem_Statistic_Click);
-            menuItem_Statistic.Visible = config.options.General.Data;
-
-#if DEBUG
-            contextMenu.MenuItems.Add("Debug...");
-#endif
-            contextMenu.MenuItems.Add(menuItem_Statistic);
-            contextMenu.MenuItems.Add(menuItem_options);
-            contextMenu.MenuItems.Add("-");
-            contextMenu.MenuItems.Add(menuItem_norest);
-            contextMenu.MenuItems.Add(menuItem_sound);
-            contextMenu.MenuItems.Add("-");
-
-            //contextMenu.MenuItems.Add(menuItem_update);
-            contextMenu.MenuItems.Add(menuItem_exit);
-
-            //this.contextMenu.MenuItems.AddRange(
-            //            new System.Windows.Forms.MenuItem[] { menuItem_norest, menuItem_sound, menuItem_exit });
-
-
-            notifyIcon = new System.Windows.Forms.NotifyIcon(components);
-            UpdateIcon("sunglasses");
-            notifyIcon.ContextMenu = contextMenu;
-            notifyIcon.Text = "Project Eye";
-            notifyIcon.Visible = true;
-            //notifyIcon.DoubleClick += new System.EventHandler(notifyIcon_DoubleClick);
-
+            }
         }
 
-        private void menuItem_norest_Click(object sender, EventArgs e)
+        private void menuItem_NoRest_Click(object sender, EventArgs e)
         {
-            var item = sender as System.Windows.Forms.MenuItem;
-            item.Checked = !item.Checked;
-            config.options.General.Noreset = item.Checked;
-            if (item.Checked)
+            var item = sender as MenuItem;
+            item.IsChecked = !item.IsChecked;
+            config.options.General.Noreset = item.IsChecked;
+            if (item.IsChecked)
             {
                 //不要提醒
                 UpdateIcon("dizzy");
@@ -143,21 +155,7 @@ namespace ProjectEye.Core.Service
         }
 
 
-        public void Remove()
-        {
-            notifyIcon.Visible = false;
-            if (components != null)
-            {
-                components.Dispose();
-            }
-        }
-        private void UpdateIcon(string name)
-        {
-            Uri iconUri = new Uri("/ProjectEye;component/Resources/" + name + ".ico", UriKind.RelativeOrAbsolute);
-            StreamResourceInfo info = Application.GetResourceStream(iconUri);
-            notifyIcon.Icon = new Icon(info.Stream);
-        }
-        private void menuItem_exit_Click(object sender, EventArgs e)
+        private void menuItem_Exit_Click(object sender, EventArgs e)
         {
             Application.Current.Shutdown();
         }
@@ -167,8 +165,19 @@ namespace ProjectEye.Core.Service
             mainService.Exit();
             Remove();
         }
+        #endregion
 
-
-
+        #region Function
+        public void Remove()
+        {
+            notifyIcon.Visible = false;
+        }
+        private void UpdateIcon(string name)
+        {
+            Uri iconUri = new Uri("/ProjectEye;component/Resources/" + name + ".ico", UriKind.RelativeOrAbsolute);
+            StreamResourceInfo info = Application.GetResourceStream(iconUri);
+            notifyIcon.Icon = new Icon(info.Stream);
+        }
+        #endregion
     }
 }
