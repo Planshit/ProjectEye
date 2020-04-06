7f37b1c720906ac275cdafeae106b839e4d2a69d
Little panda <root@thelittlepandaisbehind.com>
2020-04-06 18:04:02 +0800

    添加日期选择控件

---
+++ src/Project1.UI/Controls/DatePicker/DatePicker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Project1.UI.Controls.DatePicker
+{
+    public class DatePicker : Control
+    {
+        private ItemList itemList;
+        public DatePicker()
+        {
+            DefaultStyleKey = typeof(DatePicker);
+        }
+
+
+    }
+}
---
+++ src/Project1.UI/Controls/DatePicker/ItemList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Project1.UI.Controls.DatePicker
+{
+
+    public class ItemList : Control
+    {
+        #region 项目
+        /// <summary>
+        /// 项目
+        /// </summary>
+        public List<string> Items
+        {
+            get { return (List<string>)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items",
+                typeof(List<string>),
+                typeof(ItemList),
+                new PropertyMetadata(null, new PropertyChangedCallback(OnItemsChanged)));
+
+        private static void OnItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ItemList;
+            if (control != null)
+            {
+                control.RenderData();
+            }
+        }
+
+
+        #endregion
+
+        private WrapPanel wrapPanel;
+        public ItemList()
+        {
+            DefaultStyleKey = typeof(ItemList);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            wrapPanel = GetTemplateChild("WrapPanel") as WrapPanel;
+            RenderData();
+        }
+
+        private void RenderData()
+        {
+            wrapPanel.Children.Clear();
+            foreach (string text in Items)
+            {
+                CheckButton item = new CheckButton();
+                item.Content = text;
+                wrapPanel.Children.Add(item);
+            }
+        }
+    }
+}
