75712ddeab14350a328d95a952ed4a4fcb3614fc
thelittlepandaisbehind <root@thelittlepandaisbehind.com>
2020-10-06 20:28:26 +0800

    添加贡献者名单窗口

---
+++ src/Local/ProjectEye/ViewModels/ContributorsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ProjectEye.Core;
+using ProjectEye.Core.Service;
+using ProjectEye.Models;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+
+namespace ProjectEye.ViewModels
+{
+    public class ContributorsViewModel
+    {
+
+        public Command openurlCommand { get; set; }
+
+        public ContributorsViewModel()
+        {
+
+            openurlCommand = new Command(new Action<object>(openurlCommand_action));
+        }
+
+     
+        private void openurlCommand_action(object obj)
+        {
+            Process.Start(new ProcessStartInfo(obj.ToString()));
+        }
+
+    }
+}
